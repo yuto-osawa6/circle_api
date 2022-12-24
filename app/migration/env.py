2c96b33afa0c264e7a16576b9bbbafaf541bce35
yuto-osawa6 <62257152+yuto-osawa6@users.noreply.github.com>
2022-12-25 06:56:38 +0900

    usertable

--- app/migration/env.py
+++ app/migration/env.py
@@ -9,6 +9,7 @@ import app.db
 # import app.models.task
 import app.models.task
 import app.models.task2
+import app.models.user
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
 from app.db import Base
@@ -6,17 +6,19 @@ from app.db import Base
 
 class User(Base):
     __tablename__ = "users"
+    id = Column(Integer, primary_key = True, index=True)
+    uid = Column(Integer)
+    email = Column(String)
+    # title2 = Column(String(1024))
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String(1024))
-    title2 = Column(String(1024))
-
-    done = relationship("Done", back_populates="task")
+#     done = relationship("Done", back_populates="task")
 
 
-class Done(Base):
-    __tablename__ = "dones"
+class UserDetail(Base):
+    __tablename__ = "user_details"
 
-    id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
+    id = Column(Integer, primary_key = True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    name = Column(String(100))
 
-    task = relationship("Task", back_populates="done")
+    # task = relationship("Task", back_populates="done")
