a71171b1abc0d06be924b8a2aea986357a2b917a
yuto-osawa6 <62257152+yuto-osawa6@users.noreply.github.com>
2023-01-19 20:29:42 +0900

    Update user.py

--- app/cruds/user.py
+++ app/cruds/user.py
@@ -43,57 +43,57 @@ def get_user(res: Response, cred: HTTPAuthorizationCredentials=Depends(HTTPBeare
     res.headers['WWW-Authenticate'] = 'Bearer realm="auth_required"'
     return decoded_token
 
-async def get_or_create_user(db: AsyncSession,res: Response, cred: HTTPAuthorizationCredentials=Depends(HTTPBearer(auto_error=False))):
-    if cred is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Bearer authentication required",
-            headers={'WWW-Authenticate': 'Bearer realm="auth_required"'},
-        )
-    try:
-        print(cred)
-        decoded_token = auth.verify_id_token(cred.credentials)
+# async def get_or_create_user(db: AsyncSession,res: Response, cred: HTTPAuthorizationCredentials=Depends(HTTPBearer(auto_error=False))):
+#     if cred is None:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Bearer authentication required",
+#             headers={'WWW-Authenticate': 'Bearer realm="auth_required"'},
+#         )
+#     try:
+#         print(cred)
+#         decoded_token = auth.verify_id_token(cred.credentials)
 
-        result: Result = await db.execute(
-        select(user_model.User).filter(
-            user_model.User.uid == decoded_token['uid'],
-            user_model.User.email == decoded_token['email']
-            )
-        )
-        user: Optional[Tuple[user_model.User]] = result.first()
+#         result: Result = await db.execute(
+#         select(user_model.User).filter(
+#             user_model.User.uid == decoded_token['uid'],
+#             user_model.User.email == decoded_token['email']
+#             )
+#         )
+#         user: Optional[Tuple[user_model.User]] = result.first()
 
 
-        # print(user)
-        # print(user[0] if user is not None else None)
-        if user == None:
-            print("No")
-            # create
-            user = user_model.User(
-                uid = decoded_token['uid'],
-                email = decoded_token['email']
-            )
-            db.add(user)
-            await db.commit()
-            await db.refresh(user)
-            return user
-        pass
+#         # print(user)
+#         # print(user[0] if user is not None else None)
+#         if user == None:
+#             print("No")
+#             # create
+#             user = user_model.User(
+#                 uid = decoded_token['uid'],
+#                 email = decoded_token['email']
+#             )
+#             db.add(user)
+#             await db.commit()
+#             await db.refresh(user)
+#             return user
+#         pass
 
-        print(user)
-        print(user[0] if user is not None else None)
+#         print(user)
+#         print(user[0] if user is not None else None)
 
-        print(decoded_token['uid'])
-        print(decoded_token['email'])
+#         print(decoded_token['uid'])
+#         print(decoded_token['email'])
 
-        print("aefijaeiofjaoifje")
-    except Exception as err:
-        # 通信エラーの場合
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"Invalid authentication credentials. {err}",
-            headers={'WWW-Authenticate': 'Bearer error="invalid_token"'},
-        )
-    res.headers['WWW-Authenticate'] = 'Bearer realm="auth_required"'
-    return user
+#         print("aefijaeiofjaoifje")
+#     except Exception as err:
+#         # 通信エラーの場合
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail=f"Invalid authentication credentials. {err}",
+#             headers={'WWW-Authenticate': 'Bearer error="invalid_token"'},
+#         )
+#     res.headers['WWW-Authenticate'] = 'Bearer realm="auth_required"'
+#     return user
 
 
 
@@ -106,6 +106,7 @@ async def get_or_create_user(db: AsyncSession,decoded_token):
     #     )
     try:
         # print(cred)
+        print("kkokooo")
         # decoded_token = auth.verify_id_token(cred.credentials)
 
         result: Result = await db.execute(
@@ -133,13 +134,17 @@ async def get_or_create_user(db: AsyncSession,decoded_token):
             return user
         pass
 
-        print(user)
+        print("user:")
+        print(vars(user[0]))
+        print("aa")
+
         print(user[0] if user is not None else None)
 
         print(decoded_token['uid'])
         print(decoded_token['email'])
 
         print("aefijaeiofjaoifje")
+        # return user[0]
     except Exception as err:
         # 通信エラーの場合
         print(err)
@@ -149,4 +154,4 @@ async def get_or_create_user(db: AsyncSession,decoded_token):
             headers={'WWW-Authenticate': 'Bearer error="invalid_token"'},
         )
     # res.headers['WWW-Authenticate'] = 'Bearer realm="auth_required"'
-    return user
+    return user[0]
