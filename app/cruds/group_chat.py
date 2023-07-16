5c12fecfe218d75882314a592f0ee3ba1b08c828
yuto-osawa6 <62257152+yuto-osawa6@users.noreply.github.com>
2023-07-16 23:36:10 +0900

    fcmの追加

--- app/cruds/group_chat.py
+++ app/cruds/group_chat.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import asyncio
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db import get_db
@@ -27,12 +28,34 @@ from sqlalchemy.engine import Result
 import redis
 import json
 
+# import firebase_admin
+# from firebase_admin import credentials
+from firebase_admin import messaging
+
 redis_client = redis.Redis(host='redis', port=6379)
 # import redis
 
 #  post create_group_chat_content
 
+async def send_fcm_notification(device_token, title, body):
+    print(title,device_token,body)
+    # FCM通知メッセージの作成
+    message = messaging.Message(
+        notification=messaging.Notification(title=title, body=body),
+        token=device_token,
+    )
 
+    # FCM通知の送信
+    response = messaging.send(message)
+    print('FCM Notification sent successfully:', response)
+
+async def load_users(db: AsyncSession, group_id: int) -> List[user_model.User]:
+    query = select(user_model.User).join(user_model.User.groups).where(group_model.Group.id == group_id)
+    result = await db.execute(query)
+    users = result.scalars().all()
+    return users
+
+# check-1 errorがでたときをfrontに送ってない、
 async def create_group_chat_content(db: AsyncSession, content: group_chat_schema.GroupChatContentCreate):
     try:
         async with db.begin():
@@ -49,11 +72,41 @@ async def create_group_chat_content(db: AsyncSession, content: group_chat_schema
                 text_content=content.text_content
             )
             db.add(group_chat_content)
-        await db.commit()
 
+            # # 中間テーブルを経由して関連するユーザーを取得するクエリを実行する
+            # users_query = select(user_model.User).join(user_model.User.groups).where(group_model.Group.id == content.group_id)
+            # # users_task = asyncio.create_task(db.execute(users_query))
+            # users_result = await db.execute(users_query)
+            # # users = users_result.scalars().all()
+            # # users = users_result.all()
+            # users = [user[0] for user in users_result.all()]
+
+            
+            # # ユーザーデータの遅延ロード
+            # users = await load_users(db, content.group_id)
+            # print(users)
+            # print("users")
+            # print(users[0].id)
+
+            # ユーザーデータの遅延ロード
+            # await asyncio.gather(*[db.refresh(user) for user in users])
+
+
+        await db.commit()
         await db.refresh(group_chat_content)
-        # await db.refresh(group_chat, options=[selectinload(group_chat_model.GroupChat.content)])
+            # await db.refresh(group_chat, options=[selectinload(group_chat_model.GroupChat.content)])
         await db.refresh(group_chat)
+
+        # async with db.begin():
+        #     # 中間テーブルを経由して関連するユーザーを取得するクエリを実行する
+        #     users = db.execute(
+        #         select(user_model.User)
+        #         .join(user_model.User.groups)
+        #         .where(group_model.Group.id == content.group_id)
+        #     )
+        #     print(users)
+        #     print("users!!グループに属するユーザー情報")
+
 
         room = f"group_chanel{group_chat.group_id}"
         print(f"rooooooooom{group_chat_content}")
@@ -69,8 +122,31 @@ async def create_group_chat_content(db: AsyncSession, content: group_chat_schema
             "id": group_chat_content.id
         }
         }).encode('utf-8'))
+
+        # 通知の処理を遅延させる
+        # async with db.begin():
+            # ユーザーデータの遅延ロード
+        users = await load_users(db, content.group_id)
+        print(users)
+        print("users")
+        # print(users.id)
+            # print(users)
+        for user in users:
+            print(user)
+            print(vars(user))
+            print(user.device_token)
+            await send_fcm_notification(user.device_token, 
+                "新しいメッセージが届きました",
+                "新しいメッセージがあります。チャットを確認してください。"
+            )
+        # users = await users_task
+        # print(users.all())
+        print("users!!グループに属するユーザー情報")
+
+
         return "message 1"
-    except:
+    except Exception as e:
+        print(e)
         print("jaijfeioajioea")
     # finally:
     #     await db.close()
